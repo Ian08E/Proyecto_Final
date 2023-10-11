5c1ce12cd07498707409b56c2a591a4aa03767e9
Gabriel027-ux <gabrielcalde16@gmail.com>
2023-10-11 01:09:44 -0600

    AgregarMascota

---
+++ Proyecto_Final/Proyecto_Final/Models/Producto.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto_Final.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string ?Nombre { get; set; }
+        public string ?Descripcion { get; set; }
+        public string ?Tipo { get; set; }
+    }
+}
---
+++ Proyecto_Final/Proyecto_Final/Servicios/RepositorioProducto.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using Proyecto_Final.Models;
+
+namespace Proyecto_Final.Servicios
+{
+    public interface IRepositorioProducto
+    {
+
+    }
+    public class RepositorioProducto : IRepositorioProducto
+    {
+        private readonly string connectionString;
+
+
+
+        public RepositorioProducto(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        public async Task Crear(Producto producto)
+        {
+            using var connection = new SqlConnection(connectionString);
+        }
+    }
+}
