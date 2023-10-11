fc5048b23c7d99bb7a78bfdac956ab47811a65d7
Jmalak03 <jossidey@hotmail.es>
2023-10-11 01:17:22 -0600

    clase mascota

--- Proyecto_Final/Models/AgregarMascota.cs
+++ Proyecto_Final/Models/AgregarMascota.cs
@@ -1,14 +1,7 @@
 ﻿using System;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-public class Class1
+
+public class Mascota
 {
-	public Class1()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	}
+	
 }
---
+++ Proyecto_Final/Servicios/RepositorioProductos.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using Proyecto_Final.Models;
+
+namespace Proyecto_Final.Servicios
+{
+    public interface IRepositorioProductos
+    {
+
+    }
+    public class RepositorioProductos : IRepositorioProducto
+    {
+        private readonly string connectionString;
+
+
+
+        public RepositorioProductos(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        public async Task Agregarproducto(Productos productos)
+        {
+            using var connection = new SqlConnection(connectionString);
+        }
+    }
+}
