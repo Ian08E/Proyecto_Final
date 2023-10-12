4f2ccab5ad742f4b731d9c7432854b1c67b0146a
Jmalak03 <jossidey@hotmail.es>
2023-10-11 18:15:23 -0600

    l

--- Proyecto_Final/Program.cs
+++ Proyecto_Final/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<IRepositorioProducto, RepositorioProducto>();
+builder.Services.AddTransient<IRepositorioAnimal, RepositorioAnimal>();
 
 var app = builder.Build();
 
